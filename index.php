32b87218504dddb40ea4b094d8a221f58c6ea91b
kevin <kevinpiersoul.com>
2018-07-08 01:57:58 +0200

    Set custom default method for each controller

--- index.php
+++ index.php
@@ -23,19 +23,43 @@
     }
     
     /**
+     * Set the standard method for each controller
+     * the standard method is the method called when no explicit method is given
+     * the given value of 'default' is which method is called when no explicit method is given
+     */
+    $arrDefaultMethods = [
+        'default' => 'norm'
+    ];
+    
+    /**
      * make the controller string match the controller object: take out unwanted Characters and ad the word 'Controller' to the end
-     * Afterwards load the controller
      */
     $controllerName = preg_replace("/[^A-Za-z0-9?!]/", '', $controllerName).'Controller'; 
     require_once 'src/controller/'.$controllerName.'.php';
 
     
     //create new object of the controller and give the method name so it can be loaded in the view
+    /**
+     * get the correct method name
+     * if an explicit method name is given in the url use that on
+     * else if we declared a standard method name in $arrDefaultMethods use that one
+     * else use the default method name
+     */
     if( !empty($uri_segments[3]) ){
         $method = preg_replace("/[^A-Za-z0-9?!]/", '', $uri_segments[3]);
-    } else {
+    } else if( !empty($arrDefaultMethods[$controllerName]) ){
+        $method = $arrDefaultMethods[$controllerName];
+    } else{
         $method = 'norm';
     }
+    
+    /**
+     * Load the controller
+     * Create a new object of the controller and give the method as a parameter (needed to load in the correct view element)
+     * Call the method
+     */
+    $controllerName .= 'Controller';
+    require_once 'src/controller/'.$controllerName.'.php';
     
     $controller = new $controllerName($method);
     $controller->$method();
